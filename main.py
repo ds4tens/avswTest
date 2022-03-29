f80317938a610b23794e85de030e274b6b4152ec
Danil Lasuriya <d.r.lasuria@gmail.com>
2022-03-29 17:21:04 +0300

    mt parser working

--- main.py
+++ main.py
@@ -1,44 +1,71 @@
 from concurrent.futures import ThreadPoolExecutor
+import concurrent.futures
+import queue
 import threading
 import requests
 from bs4 import SoupStrainer, BeautifulSoup
 from requests import  exceptions
 from time import sleep
-from itertools import repeat
 
+# TODO перекинуть общий список и base url в классы
 Q = []
 BASE_URL = 'https://google.com'
-r = requests.get('https://google.com')
 
-only_a_tags = SoupStrainer("a")
-soup = BeautifulSoup(r.content, 'lxml')
-for a in soup.find_all(only_a_tags, href=True):
-    a['href']
 
+class Urls:
+    def __init__(self, parent_url, work_url, depth) -> None:
+        self.parent_url = parent_url
+        self.work_url = work_url
+        self.depth = depth
+        self.child_urls = []
+        self.status = False
 
 class Pool:
-    def __init__(self, max_workers, job) -> None:
-        self.workers = max_workers
-        self.job = job
 
-    def form_new_tasks(self, links):
-        for link in links:
-            self.job.append(link)
+    MAX_DEPTH = 2
+
+    def __init__(self, max_workers, URL) -> None:
+        self.workers = max_workers
+        self.job = queue.SimpleQueue()
+        self.job.put(URL)
+
+    def _form_new_tasks(self, url_obj):
+        if not url_obj.status:
+            self.job.put(url_obj.work_url)
+        else:
+            for url in url_obj.child_urls:
+                self.job.put(Urls(url_obj.work_url, url, url_obj.depth + 1))
+                
     
     def run(self):
-        flag = 2
+        for _ in range(self.MAX_DEPTH):     
+            with ThreadPoolExecutor(max_workers=self.workers) as executor:
+                futures = []
+                sentinel = object()
+                for task in iter(self.job.get, sentinel):
+                    
+                    parser = Parser(task)
+                    futures.append(executor.submit(parser.parse))
+                    
+                    if self.job.qsize() == 0:
+                        break
+                
+                for future in concurrent.futures.as_completed(futures):
+                    self._form_new_tasks(future.result())
+                sleep(1)
+            print('NEXT LEVEL')
         
-        with ThreadPoolExecutor(max_workers=self.workers) as executor:
-            gen = executor.map(Parser.parse, repeat(a),  self.job)
-        for g in gen:
-            self.form_new_tasks(g)
+
+        print(self.job.qsize())
 
 class Parser:
 
-    def __init__(self, url, name='thr') -> None:
+    def __init__(self, url: object, name='thr') -> None:
         self.name = name
-        self.url = url
-    
+        self.url = url.work_url
+        self.url_obj = url
+        
+
     @staticmethod
     def _connect(url):
         print(url)
@@ -63,8 +90,8 @@ class Parser:
         return response 
         
 
-    def parse(self, url):
-        response = Parser('')._connect(url)
+    def parse(self):
+        response = self._connect(self.url)
         if response is not None and response.status_code == 200:
             next_gen = []
             only_a_tags = SoupStrainer("a")
@@ -81,16 +108,13 @@ class Parser:
                     Q.append(link)
                     next_gen.append(link)
                 lock.release()
-            return next_gen        
+            self.url_obj.child_urls = next_gen
 
-        return ()
+        self.url_obj.status = True                   
+        return self.url_obj
 
 
-lst = ['https://www.google.ru/imghp?hl=ru&tab=wi', 'https://maps.google.ru/maps?hl=ru&tab=wl', 'https://play.google.com/?hl=ru&tab=w8', 'https://www.youtube.com/?gl=RU&tab=w1', 'https://news.google.com/?tab=wn', 'https://mail.google.com/mail/?tab=wm', 'https://drive.google.com/?tab=wo', 'https://www.google.ru/intl/ru/about/products?tab=wh', 'http://www.google.ru/history/optout?hl=ru', '/preferences?hl=ru', 'https://accounts.google.com/ServiceLogin?hl=ru&passive=true&continue=https://www.google.com/&ec=GAZAAQ', '/advanced_search?hl=ru&authuser=0', 'http://www.google.ru/intl/ru/services/', '/intl/ru/about.html', 'https://www.google.com/setprefdomain?prefdom=RU&prev=https://www.google.ru/&sig=K_-a8iAohFX3vwfmAvR4WW_aSAQ18%3D', '/intl/ru/policies/privacy/', '/intl/ru/policies/terms/']
-
+lst = Urls(None ,'https://www.google.ru/imghp?hl=ru&tab=wi', 0)
 pool = Pool(2, lst)
 pool.run()
 print(Q)
-'''parser = Parser('edu', 'https://google.com/')
-print(parser.parse())
-print()'''
